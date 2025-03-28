b12cf0be669750842a69f03a1b77c30885682a25
Shadow0003 <121765587+Shadow0003@users.noreply.github.com>
2025-03-28 16:42:22 +0530

    Add files via upload

---
+++ Table over 500K.lua
@@ -0,0 +1,22 @@
+﻿
+local _shad = _G['gg']
+ 
+local function removeResultsInBatches(batchSize, maxResults)
+    local totalResults = _shad.getResultsCount()
+    
+    while totalResults > 0 and maxResults > 0 do
+
+        local currentBatchSize = math.min(batchSize, maxResults)
+        local results = _shad.getResults(currentBatchSize)
+        _shad.removeResults(results)
+
+        totalResults = _shad.getResultsCount()
+        maxResults = maxResults - currentBatchSize
+    end
+end
+
+_shad.searchPointer(0)
+
+local batchSize = 100000
+local maxResults = 500000
+removeResultsInBatches(batchSize, maxResults)
